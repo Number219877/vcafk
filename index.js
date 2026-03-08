373a67a41339c978337fac47fa30555c8e04b6f6
Number219877 <number2.19877@gmail.com>
2026-03-08 21:04:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,24 +9,46 @@ const TOKEN = process.env.TOKEN;
 const CHANNEL_ID = process.env.CHANNEL_ID;
 const GUILD_ID = process.env.GUILD_ID;
 
-client.on("ready", async () => {
+let connection = null;
 
+async function joinVC() {
+    try {
+        const channel = await client.channels.fetch(CHANNEL_ID);
+
+        connection = joinVoiceChannel({
+            channelId: CHANNEL_ID,
+            guildId: GUILD_ID,
+            adapterCreator: channel.guild.voiceAdapterCreator,
+            selfDeaf: true
+        });
+
+        console.log("Joined voice channel");
+    } catch (err) {
+        console.log("Join error:", err);
+    }
+}
+
+client.on("ready", async () => {
     console.log("Logged in as " + client.user.tag);
 
-    const channel = await client.channels.fetch(CHANNEL_ID);
+    await joinVC();
 
-    joinVoiceChannel({
-        channelId: CHANNEL_ID,
-        guildId: GUILD_ID,
-        adapterCreator: channel.guild.voiceAdapterCreator,
-        selfDeaf: true,
-    });
+    // Reconnect every 1 hour
+    setInterval(async () => {
+        console.log("Reconnecting to VC...");
 
-    console.log("Joined VC");
+        try {
+            if (connection) {
+                connection.destroy();
+            }
+        } catch {}
+
+        await joinVC();
+
+    }, 60 * 60 * 1000);
 });
 
 client.login(TOKEN);
 
 app.get("/", (req,res)=>res.send("running"));
-
 app.listen(3000);
