71da34fcc3e00dbf18b10526e732d6c0a1056a58
Number219877 <number2.19877@gmail.com>
2026-03-08 22:13:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,20 +9,51 @@ const TOKEN = process.env.TOKEN;
 const CHANNEL_ID = process.env.CHANNEL_ID;
 const GUILD_ID = process.env.GUILD_ID;
 
+let connection;
+
+async function joinVC() {
+    try {
+
+        const channel = await client.channels.fetch(CHANNEL_ID);
+
+        connection = joinVoiceChannel({
+            channelId: CHANNEL_ID,
+            guildId: GUILD_ID,
+            adapterCreator: channel.guild.voiceAdapterCreator,
+            selfDeaf: true
+        });
+
+        console.log("Joined VC");
+
+    } catch (err) {
+        console.log("VC Join Error:", err);
+    }
+}
+
 client.on("ready", async () => {
 
     console.log("Logged in as " + client.user.tag);
 
-    const channel = await client.channels.fetch(CHANNEL_ID);
+    await joinVC();
 
-    joinVoiceChannel({
-        channelId: CHANNEL_ID,
-        guildId: GUILD_ID,
-        adapterCreator: channel.guild.voiceAdapterCreator,
-        selfDeaf: true,
-    });
+    setInterval(async () => {
 
-    console.log("Joined VC");
+        console.log("1 hour completed → leaving VC");
+
+        try {
+            if (connection) {
+                connection.destroy();
+            }
+        } catch {}
+
+        console.log("Waiting 1 minute before rejoining");
+
+        setTimeout(async () => {
+            await joinVC();
+        }, 60000);
+
+    }, 3600000);
+
 });
 
 client.login(TOKEN);
